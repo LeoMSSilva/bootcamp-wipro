e05f03b41aa902fe6d7de1f63b83b97d366a9499
LeoMSSilva <l.eom1994@hotmail.com>
2022-04-08 18:44:43 -0300

    Feature: OOP exercise one solved

---
+++ basicJavaActivities/src/POO1/RemoteControl.java
@@ -0,0 +1,42 @@
+package POO1;
+
+public class RemoteControl {
+	private int volume;
+	private int channel;
+
+	public RemoteControl() {
+		this.volume = 50;
+		this.channel = 1;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+
+	public int getChannel() {
+		return channel;
+	}
+
+	public void setChannel(int channel) {
+		this.channel = channel;
+		System.out.printf("\nVocê está assistindo ao canal %d.", this.channel);
+	}
+
+	public void increaseOrDecreaseVolume (boolean upOrDown) {
+		this.volume = upOrDown == true ? this.volume+1 : this.volume-1;
+		System.out.printf("\nO volume da televisão está em %d.", this.volume);
+	};
+
+	public void increaseOrDecreaseChannel (boolean upOrDown) {
+		this.channel = upOrDown == true ? this.channel+1 : this.channel-1;
+		System.out.printf("\nVocê está assistindo ao canal %d.", this.channel);
+	};
+
+	public void showVolumeAndChannel (){
+		System.out.printf("\nVocê está assistindo ao canal %d e o volume da televisão está em %d.", this.channel, this.volume);
+	}
+}
---
+++ basicJavaActivities/src/POO1/Television.java
@@ -0,0 +1,17 @@
+package POO1;
+
+public class Television {
+	public static void main(String[] args) {
+		RemoteControl remoteControl = new RemoteControl();
+		remoteControl.showVolumeAndChannel();
+		remoteControl.increaseOrDecreaseVolume(false);
+		remoteControl.increaseOrDecreaseVolume(false);
+		remoteControl.increaseOrDecreaseVolume(true);
+		remoteControl.increaseOrDecreaseChannel(false);
+		remoteControl.increaseOrDecreaseChannel(true);
+		remoteControl.increaseOrDecreaseChannel(true);
+		remoteControl.setChannel(60);
+		remoteControl.showVolumeAndChannel();
+	}
+
+}
