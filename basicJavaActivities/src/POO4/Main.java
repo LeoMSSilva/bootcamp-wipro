2aa0253b765a6578c09a7ef18ae8822b84d30b77
LeoMSSilva <l.eom1994@hotmail.com>
2022-04-08 16:35:16 -0300

    Refactor: language change in results presentation

--- basicJavaActivities/src/POO4/Main.java
+++ basicJavaActivities/src/POO4/Main.java
@@ -6,20 +6,20 @@ public class Main {
 		Teacher Maria = new Teacher();
 		Maria.setNome("Maria");
 		Maria.setCpf("12345678910");
-		Maria.setSalario(1200);
+		Maria.setSalario(1200.00);
 
 		Developer Marta = new Developer();
 		Marta.setNome("Marta");
-		Marta.setCpf("21");
-		Marta.setSalario(2000);
+		Marta.setCpf("34567891011");
+		Marta.setSalario(2000.00);
 
-		System.out.printf("Salário de %s sem aumento: %.2f\n", Maria.getNome(), Maria.getSalario());
+		System.out.printf("%s's salary without increase: $ %.2f\n", Maria.getNome(), Maria.getSalario());
 		Maria.bonus();
-		System.out.printf("Salário de %s com aumento: %.2f\n", Maria.getNome(), Maria.getSalario());
+		System.out.printf("%s's salary with increase: $ %.2f\n", Maria.getNome(), Maria.getSalario());
 
-		System.out.printf("Salário de %s sem aumento: %.2f\n", Marta.getNome(), Marta.getSalario());
+		System.out.printf("%s's salary without increase: $ %.2f\n", Marta.getNome(), Marta.getSalario());
 		Marta.bonus();
-		System.out.printf("Salário de %s com aumento: %.2f\n", Marta.getNome(), Marta.getSalario());
+		System.out.printf("%s's salary with increase: $ %.2f\n", Marta.getNome(), Marta.getSalario());
 	}
 
 }
